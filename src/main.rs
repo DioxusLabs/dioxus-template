cec3c1340bf55815b1773f5dfe000642b8818684
mrxiaozhuox <mrxzx@wwsg18.top>
2022-01-24 10:28:33 +0800

    fix: code structure

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,12 @@ fn main() {
 
 fn app(cx: Scope) -> Element {
 
-    let platform = if utils::platform::is_wasm() { "WebAssembly" } else { "Desktop" };
-
     cx.render(rsx! (
         div {
             style: "text-align: center;",
             h1 { "🌗 Dioxus 🚀" }
             h3 { "Frontend that scales." }
             p { "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
-            ul {
-                li { "runtime platform: {platform}" }
-            }
         }
     ))
 }
