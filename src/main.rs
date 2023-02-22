6ca81ba7e4dbf18135b8aa2043303350937b58e0
Jonathan Kelley <jkelleyrtp@gmail.com>
2023-02-22 11:57:36 -0800

    bump dioxus version to 0.3

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ fn main() {
     wasm_logger::init(wasm_logger::Config::default());
     console_error_panic_hook::set_once();
 
-    dioxus::web::launch(app); 
+    dioxus_web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
-
     cx.render(rsx! (
         div {
             style: "text-align: center;",
