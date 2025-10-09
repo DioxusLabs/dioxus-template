d5eb5886aa0f601a7887669190eeb074fce74300
Jonathan Kelley <jkelleyrtp@gmail.com>
2025-10-09 14:00:29 -0700

    wire up server feature across crates

--- Workspace/packages/api/src/lib.rs
+++ Workspace/packages/api/src/lib.rs
@@ -2,7 +2,7 @@
 use dioxus::prelude::*;
 
 /// Echo the user input on the server.
-#[server(Echo)]
+#[get("/api/echo")]
 pub async fn echo(input: String) -> Result<String, ServerFnError> {
     Ok(input)
 }
